870b334753265a91521b6c18040637265ccb2d26
Reyter Onán Garzón Cintra <rogarzonc87@gmail.com>
2021-01-01 22:21:02 -0500

    Primer commit

---
+++ app/src/main/java/com/example/puppy/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.puppy;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+    }
+}
