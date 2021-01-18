0b354ea65d378ff4486ce634316ef2175d6a0b68
Reyter Onán Garzón Cintra <rogarzonc87@gmail.com>
2021-01-18 13:19:29 -0500

    Algunas correciones hechas para enviar el correo electrónico

--- app/src/main/java/com/example/puppy/activities/ContactActivity.java
+++ app/src/main/java/com/example/puppy/activities/ContactActivity.java
@@ -1,11 +1,13 @@
 package com.example.puppy.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.puppy.R;
 
@@ -13,7 +15,6 @@ import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -40,29 +41,44 @@ public class ContactActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.btSend);
         button.setOnClickListener(v -> {
-            String smtpHostServer = "smtp.mail.com";
-            String emailAddress = etEmailAddress.getText().toString();
-            String emailText = etEmailText.getText().toString();
+            new Thread(() -> {
+                String smtpHostServer = "smtp.gmail.com";
+                String emailAddress = etEmailAddress.getText().toString();
+                String emailText = etEmailText.getText().toString();
 
-            Properties prop = System.getProperties();
-            prop.put("mail.smtp.host", smtpHostServer);
+                Properties props = System.getProperties();
+                props.put("mail.smtp.host", smtpHostServer);
 
-            Session session = Session.getInstance(prop, null);
-
-            try {
-                MimeMessage  msg = new MimeMessage(session);
-                msg.setFrom(new InternetAddress("mymail@gmail.com"));
-                InternetAddress[] addresses = {new InternetAddress(emailAddress)};
-                msg.setRecipients(Message.RecipientType.TO, addresses);
-                msg.setSubject("Pet App Test");
-                msg.setSentDate(new Date());
-                msg.setText(emailText);
-
-                Transport.send(msg);
-            }catch (MessagingException e){
-                System.out.println("Exception sending the email");
-                e.printStackTrace();
-            }
+                Session session = Session.getInstance(props, null);
+                sendMessage(session, emailAddress, "Email test", emailText);
+            }).start();
         });
+    }
+
+    public void sendMessage(@NonNull Session session, String toEmail, String subject, String body) {
+        try {
+            MimeMessage msg = new MimeMessage(session);
+            msg.addHeader("Content-Type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("no_reply@example.com", "No Reply"));
+            msg.setReplyTo(InternetAddress.parse("no_reply@example.com", true));
+
+            msg.setSubject(subject, "UTF-8");
+            msg.setText(body, "UTF-8");
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(toEmail, false));
+
+            Transport.send(msg);
+            System.out.println("Message sent successfully");
+            Toast.makeText(this, "Message sent successfully",
+                    Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error sending message: " + e.getMessage());
+        }
     }
 }
